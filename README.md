70a4d64f4dcb5dea3ca008ec2e63633b48ad47a0
pinkhominid <pinkhominid@birdbomb.com>
2020-01-25 13:48:39 -0500

    Rename pkg; add contentType; normalize text() and json()

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# Network Listener
+# Network Event
 
-Browser network listener
+A global normalized event for browser network requests (fetch & XHR).
 
 ```js
-import { EVENT_TYPE } from './network-listener.js';
+import { NETWORK_EVENT, PENDING_STATE, COMPLETE_STATE } from './network-event.js';
 
-window.addEventListener(EVENT_TYPE, e => {
-  console.log(EVENT_TYPE, e.detail);
+window.addEventListener(NETWORK_EVENT, async e => {
+  switch (e.detail.state) {
+    case PENDING_STATE: console.log(e.detail); break;
+    case COMPLETE_STATE: console.log(e.detail, await e.detail.json()); break;
+  }
 });
 
 fetch('https://jsonplaceholder.typicode.com/users');
@@ -14,4 +17,16 @@ fetch('https://jsonplaceholder.typicode.com/users');
 const req = new XMLHttpRequest();
 req.open('get', 'https://jsonplaceholder.typicode.com/users');
 req.send();
+```
+
+```sh
+Object { state: "pending", url: "https://jsonplaceholder.typicode.com/users" }
+
+Object { state: "pending", url: "https://jsonplaceholder.typicode.com/users" }
+
+Object { state: "complete", url: "https://jsonplaceholder.typicode.com/users", status: 200, "content-type": "application/json; charset=utf-8", text: send(), json: send() }
+Array(10) [ {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…} ]
+
+Object { state: "complete", url: "https://jsonplaceholder.typicode.com/users", status: 200, "content-type": "application/json; charset=utf-8", text: fetch(), json: fetch() }
+Array(10) [ {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…} ]
 ```
--- network-event.js
+++ network-event.js
@@ -1,10 +1,4 @@
-/* TODO
- * - make sure timeouts work
- * - safeguard against non-text responses
- * - make dispatcher pluggable
- */
-
-export const EVENT_TYPE = 'network-request-state-change';
+export const NETWORK_EVENT = 'network-event';
 export const PENDING_STATE = 'pending';
 export const COMPLETE_STATE = 'complete';
 
@@ -13,9 +7,15 @@ fetch = function(resource, init) {
   const url = resource.url || resource;
   dispatchPending(url);
   return origFetch.apply(this, arguments).then(
-    async origResp => {
+    origResp => {
       const resp = origResp.clone(); // clone so we can consume text stream w/o affecting orig
-      dispatchComplete(resp.url, resp.status, await resp.text());
+      dispatchComplete(
+        resp.url,
+        resp.status,
+        resp.headers.get('content-type'),
+        async () => resp.text(),
+        async () => resp.json()
+      );
       return origResp;
     },
     err => {
@@ -36,21 +36,34 @@ xhrProto.send = function() {
   dispatchPending(this._requestURL);
   this.addEventListener('loadend', () => {
     const url = this.responseURL || this._requestURL;
-    dispatchComplete(url, this.status, this.response)
+    dispatchComplete(
+      url,
+      this.status,
+      this.getResponseHeader('content-type'),
+      async () => Promise.resolve(this.response),
+      async () => Promise.resolve(JSON.parse(this.response))
+    );
   }, { once: true });
   origSend.apply(this, arguments);
 };
 
 function dispatchPending(url) {
-  dispatch({ state: PENDING_STATE, url: absUrl(url) })
+  dispatch({ state: PENDING_STATE, url: absUrl(url) });
 }
 
-function dispatchComplete(url, status, responseText) {
-  dispatch({ state: COMPLETE_STATE, url: absUrl(url), status, responseText })
+function dispatchComplete(url, status, contentType, text, json) {
+  dispatch({
+    state: COMPLETE_STATE,
+    url: absUrl(url),
+    status,
+    'content-type': contentType,
+    text,
+    json
+  });
 }
 
 function dispatch(detail) {
-  const event = new CustomEvent(EVENT_TYPE, { detail });
+  const event = new CustomEvent(NETWORK_EVENT, { detail });
   dispatchEvent(event);
 }
 
